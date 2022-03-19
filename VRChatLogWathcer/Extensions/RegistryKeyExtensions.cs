29b77e443d0a54e7b8c3308b1a830326334ae590
rioil <rioil.dev@gmail.com>
2022-03-19 14:53:38 +0900

    スタートアップ登録機能を実装

---
+++ VRChatLogWathcer/Extensions/RegistryKeyExtensions.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.Security;
+
+namespace VRChatLogWathcer.Extensions
+{
+    /// <summary>
+    /// RegistryKeyの拡張メソッドクラス
+    /// </summary>
+    public static class RegistryKeyExtensions
+    {
+        /// <summary>
+        /// 書き込みアクセスが必要であるかを指定して，サブキーをオープンします．
+        /// </summary>
+        /// <param name="registryKey">キー</param>
+        /// <param name="name">名前</param>
+        /// <param name="writable">書き込みアクセスが必要であればtrue</param>
+        /// <param name="throwOnSecurityError">権限不足時に例外を送出する場合true</param>
+        /// <returns>オープンしたサブキー</returns>
+        public static RegistryKey? OpenSubKey(this RegistryKey registryKey, string name, bool writable, bool throwOnSecurityError)
+        {
+            try
+            {
+                return registryKey.OpenSubKey(name, writable);
+            }
+            catch (SecurityException)
+            {
+                if (throwOnSecurityError) { throw; }
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// キーの指定した名前の値を取得します．
+        /// </summary>
+        /// <param name="registryKey">キー</param>
+        /// <param name="name">名前</param>
+        /// <param name="value">値</param>
+        /// <returns>成功すればtrue</returns>
+        public static bool TryGetValue(this RegistryKey registryKey, string name, [NotNullWhen(true)] out string? value)
+        {
+            try
+            {
+                value = registryKey.GetValue(name) as string;
+                return value is not null;
+            }
+            catch (Exception ex) when (ex is SecurityException || ex is IOException || ex is UnauthorizedAccessException)
+            {
+                value = null;
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// キーの指定した名前の値を設定します．
+        /// </summary>
+        /// <param name="registryKey">キー</param>
+        /// <param name="name">名前</param>
+        /// <param name="value">値</param>
+        /// <param name="registryValueKind">値の種類</param>
+        /// <returns>成功すればtrue</returns>
+        public static bool TrySetValue(this RegistryKey registryKey, string name, object value, RegistryValueKind registryValueKind)
+        {
+            try
+            {
+                registryKey.SetValue(name, value, registryValueKind);
+                return true;
+            }
+            catch (Exception ex) when (ex is ArgumentException || ex is UnauthorizedAccessException || ex is SecurityException)
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// キーの指定した名前の値を削除します．
+        /// </summary>
+        /// <param name="registryKey">キー</param>
+        /// <param name="name">名前</param>
+        /// <returns>成功すればtrue．指定した名前の値が存在しない場合もtrue．</returns>
+        public static bool TryDeleteValue(this RegistryKey registryKey, string name)
+        {
+            try
+            {
+                registryKey.DeleteValue(name, false);
+                return true;
+            }
+            catch (Exception ex) when (ex is ArgumentException || ex is SecurityException || ex is UnauthorizedAccessException)
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ VRChatLogWathcer/Models/StartupRegister.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Win32;
+using System.Windows.Forms;
+using VRChatLogWathcer.Extensions;
+
+namespace VRChatLogWathcer.Models
+{
+    /// <summary>
+    /// スタートアップ起動の登録・削除などを行います．
+    /// </summary>
+    internal class StartupRegister
+    {
+        /// <summary>
+        /// スタートアップ起動キー
+        /// </summary>
+        private const string RunKey = @"Software\Microsoft\Windows\CurrentVersion\Run";
+
+        /// <summary>
+        /// このEXEのパス
+        /// </summary>
+        /// <remarks>ダブルクォートで囲まれています．</remarks>
+        private static readonly string ExePath = $"\"{Application.ExecutablePath}\"";
+
+        /// <summary>
+        /// スタートアップ起動登録を行います．
+        /// </summary>
+        /// <returns>成功すればtrue．失敗すればfalse．</returns>
+        public static bool Register()
+        {
+            using var key = Registry.CurrentUser.OpenSubKey(RunKey, true, false);
+            if (key is null) { return false; }
+
+            return key.TrySetValue(Application.ProductName, ExePath, RegistryValueKind.String);
+        }
+
+        /// <summary>
+        /// スタートアップ起動登録を削除します．
+        /// </summary>
+        /// <returns>成功した場合と登録されていなかった場合はtrue．削除に失敗した場合はfalse．</returns>
+        public static bool Unregister()
+        {
+            using var key = Registry.CurrentUser.OpenSubKey(RunKey, true, false);
+            if (key is null) { return true; }
+
+            return key.TryDeleteValue(Application.ProductName);
+        }
+
+        /// <summary>
+        /// スタートアップ起動登録されているかを確認します．
+        /// </summary>
+        /// <returns>登録されていればtrue</returns>
+        public static bool IsRegistered()
+        {
+            using var key = Registry.CurrentUser.OpenSubKey(RunKey, true, false);
+            if (key is null) { return false; }
+
+            return key.TryGetValue(Application.ProductName, out string? value) && value == ExePath;
+        }
+    }
+}
