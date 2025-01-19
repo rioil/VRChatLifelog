270cfc99204c7a45a3f0d1bc42c7b43138dbc764
rioil <rioil.dev@gmail.com>
2025-01-19 14:17:36 +0900

    PlayerIdを入退出ログに追加

--- VRChatLifelog/Data/JoinLeaveHistory.cs
+++ VRChatLifelog/Data/JoinLeaveHistory.cs
@@ -2,28 +2,21 @@
 
 namespace VRChatLifelog.Data
 {
-    public class JoinLeaveHistory
+    public class JoinLeaveHistory(string playerName, string playerId, DateTime joined, bool isLocal, int locationHistoryId)
     {
-        public JoinLeaveHistory(string playerName, DateTime joined, bool isLocal, LocationHistory location)
-            : this(playerName, joined, isLocal, location.Id)
+        public JoinLeaveHistory(string playerName, string playerId, DateTime joined, bool isLocal, LocationHistory location)
+            : this(playerName, playerId, joined, isLocal, location.Id)
         {
-        }
-
-        public JoinLeaveHistory(string playerName, DateTime joined, bool isLocal, int locationHistoryId)
-        {
-            PlayerName = playerName;
-            Joined = joined;
-            IsLocal = isLocal;
-            LocationHistoryId = locationHistoryId;
         }
 
         public int Id { get; set; }
-        public string PlayerName { get; set; }
-        public DateTime Joined { get; set; }
+        public string PlayerName { get; set; } = playerName;
+        public string? PlayerId { get; set; } = playerId;
+        public DateTime Joined { get; set; } = joined;
         public DateTime? Left { get; set; }
-        public bool IsLocal { get; set; }
+        public bool IsLocal { get; set; } = isLocal;
 
-        public int LocationHistoryId { get; set; }
+        public int LocationHistoryId { get; set; } = locationHistoryId;
         public LocationHistory LocationHistory { get; set; } = default!;
     }
 }
